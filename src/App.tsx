a70fae3b8fa7db7baf727de69ab21e0cf5213d11
Sparkenstein <zetabytes.pp@gmail.com>
2022-01-25 20:55:05 +0530

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import { Welcome } from "./Components/Welcome";
 import { Navbar } from "./Layout/Navbar";
 
 function App() {
   return (
-    <Box h="full">
+    <Flex h="full" justifyContent={"flex-start"}>
       <Navbar />
-    </Box>
+      <Box flex="1">
+        <Routes>
+          <Route path="/" element={<Welcome />}></Route>
+        </Routes>
+      </Box>
+    </Flex>
   );
 }
 
