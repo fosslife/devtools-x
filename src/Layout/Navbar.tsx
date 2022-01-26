75c85b5cd890a1386e7ecadfb38453fc8b768583
Sparkenstein <zetabytes.pp@gmail.com>
2022-01-26 12:30:25 +0530

    feat: navbar using cards

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,11 +1,28 @@
-import { Box, Flex, Heading, Input } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Icon, Text, Input } from "@chakra-ui/react";
+import { Card } from "../Components/Card";
+import { MdAnchor } from "react-icons/md";
 
 export const Navbar = () => {
   return (
-    // TODO: should remove fixex width?
-    <Flex h="full" w="56" bg="gray.700" shadow={"inner"} p="3">
-      <Input placeholder="Search..." size={"sm"} />
+    <Flex
+      h="full"
+      bg="gray.700"
+      shadow={"inner"}
+      p="3"
+      flexDirection={"column"}
+      overflow={"scroll"}
+      pos={"relative"}
+    >
+      <Input pos={"sticky"} placeholder="Search..." size={"sm"} />
+      <Box borderBottom={"1px solid tomato"} p="1.5">
+        {" "}
+      </Box>
+      <Box mt="1.5">
+        <Card linkto="/json-formatter">
+          <Icon as={MdAnchor}></Icon>
+          <Text>Json formatter</Text>
+        </Card>
+      </Box>
     </Flex>
   );
 };
