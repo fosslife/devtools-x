99ae28e7713d879fbe5e2da6283d83de16ba8e3e
Sparkenstein <zetabytes.pp@gmail.com>
2022-01-26 12:28:08 +0530

    route: json-formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./Components/Welcome";
+import { JsonFormatter } from "./Features/Json/JsonFormatter";
 import { Navbar } from "./Layout/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Box flex="1">
         <Routes>
           <Route path="/" element={<Welcome />}></Route>
+          <Route path="/json-formatter" element={<JsonFormatter />}></Route>
         </Routes>
       </Box>
     </Flex>
