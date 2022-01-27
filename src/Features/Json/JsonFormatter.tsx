938c90490938c552e3bf57b380dc0f0f80ee85a6
Sparkenstein <zetabytes.pp@gmail.com>
2022-01-27 10:10:11 +0530

    feat: context menu command

--- src/Features/Json/JsonFormatter.tsx
+++ src/Features/Json/JsonFormatter.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
-import Editor, { type OnMount } from "@monaco-editor/react";
+import { Tooltip, Button, Flex } from "@chakra-ui/react";
+import Editor, { DiffEditor, type OnMount } from "@monaco-editor/react";
 import { useRef } from "react";
 
 const def = {
@@ -20,6 +20,27 @@ export const JsonFormatter = () => {
 
   const onMount: OnMount = (editor, monaco) => {
     console.log("Mounted", monaco);
+    // minify command
+    editor.addAction({
+      id: "minify-json",
+      label: "Minify json",
+      //FIXME: keybindings not working
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM],
+      // A precondition for this action.
+      precondition: null,
+
+      // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+      keybindingContext: null,
+
+      contextMenuGroupId: "navigation",
+
+      contextMenuOrder: 1.5,
+      run: function (ed: any) {
+        ed.setValue(JSON.stringify(def));
+      },
+    });
+
+    // ref
     editorRef.current = editor;
   };
   return (
@@ -31,28 +52,33 @@ export const JsonFormatter = () => {
         }}
         defaultLanguage="json"
         theme="vs-dark"
-        height={"90vh"}
-        width={"85%"}
+        height={"93vh"}
+        width={"100%"}
         defaultValue={JSON.stringify(def, null, 2)}
         onMount={onMount}
       />
       <Flex gap={5} flexDirection={"column"}>
-        <Button
-          onClick={() => {
-            console.log(editorRef.current);
-            editorRef.current?.setValue(JSON.stringify(def, null, 2));
-          }}
-        >
-          Format
-        </Button>
-        <Button
-          onClick={() => {
-            console.log(editorRef.current);
-            editorRef.current?.setValue(JSON.stringify(def));
-          }}
-        >
-          Minify
-        </Button>
+        <Tooltip label="Alt+Shift+F" openDelay={500}>
+          <Button
+            size={"sm"}
+            onClick={() => {
+              editorRef.current?.setValue(JSON.stringify(def, null, 2));
+            }}
+          >
+            Format
+          </Button>
+        </Tooltip>
+
+        <Tooltip label="Alt+Shift+M" openDelay={500}>
+          <Button
+            size={"sm"}
+            onClick={() => {
+              editorRef.current?.setValue(JSON.stringify(def));
+            }}
+          >
+            Minify
+          </Button>
+        </Tooltip>
       </Flex>
     </Flex>
   );
