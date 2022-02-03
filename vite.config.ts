6ff2f5e27aba3297f01ae239b80d76005029f6d6
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-03 12:17:24 +0530

    conf: build targets for toplevel await

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    target: ["chrome90", "edge90", "esnext", "firefox90", "safari15.1"],
+  },
+});
