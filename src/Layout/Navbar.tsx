2df5323e94aa65474786b3badfd4a767884b25d7
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-05 00:08:34 +0530

    feat: generate random text (pass)

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Icon, Text, Input } from "@chakra-ui/react";
 import { Card } from "../Components/Card";
 import { MdAnchor } from "react-icons/md";
 import { FiHash } from "react-icons/fi";
+import { FaRandom } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
@@ -30,6 +31,13 @@ export const Navbar = () => {
         <Card linkto="/hash">
           <Icon as={FiHash}></Icon>
           <Text>Hashing tools</Text>
+        </Card>
+      </Box>
+
+      <Box mt="1.5">
+        <Card linkto="/random">
+          <Icon as={FaRandom}></Icon>
+          <Text>Random Text</Text>
         </Card>
       </Box>
     </Flex>
