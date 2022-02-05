501d49d44e6c91a27a55fdc3e1058f147a875ad6
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-05 13:00:56 +0530

    conf: cleanup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,28 +1,8 @@
 module.exports = {
   semi: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: false,
   printWidth: 80,
   tabWidth: 2,
-  endOfLine: "lf",
+  endOfLine: 'lf',
 };
-
-// {
-//   "arrowParens": "always",
-//   "bracketSpacing": true,
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "jsxSingleQuote": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "requirePragma": false,
-//   "semi": true,
-//   "singleQuote": false,
-//   "tabWidth": 2,
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-//   "printWidth": 100
-// }
