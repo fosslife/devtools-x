cdc3c90b72e2e12b684921cf2fde1bb1abfd501f
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-05 12:51:36 +0530

    conf: prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+};
+
+// {
+//   "arrowParens": "always",
+//   "bracketSpacing": true,
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxBracketSameLine": false,
+//   "jsxSingleQuote": false,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "semi": true,
+//   "singleQuote": false,
+//   "tabWidth": 2,
+//   "trailingComma": "es5",
+//   "useTabs": false,
+//   "vueIndentScriptAndStyle": false,
+//   "printWidth": 100
+// }
