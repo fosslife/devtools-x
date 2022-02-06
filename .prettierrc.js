506046a9dde233b32c2287a412269b754711807b
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-06 10:49:29 +0530

    fix: quotes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   semi: true,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   singleQuote: false,
   printWidth: 80,
   tabWidth: 2,
-  endOfLine: 'lf',
+  endOfLine: "lf",
 };
