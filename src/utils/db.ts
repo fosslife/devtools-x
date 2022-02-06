2d11d3e60b9338e4558f3ea55311a9a859edc665
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-06 20:14:26 +0530

    fix(utils): crash when launching app for the first time

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -9,7 +9,6 @@ const getConfFile = async () => {
   return path.join(await getAppDir(), "conf.json");
 };
 
-//TODO: Json parsing strigify is slow. look for possible alternatives - p5 since conf file will be really small
 class CustomAdaptor {
   // Optional: your adapter can take arguments
   constructor() {}
@@ -34,11 +33,49 @@ class CustomAdaptor {
   }
 }
 
+const dir = await path.appDir();
+try {
+  await fs.readDir(dir);
+  console.log("dir exists");
+  try {
+    await fs.readTextFile(await path.join(dir, "conf.json"));
+  } catch (e) {
+    console.error("conf file doesnt exist, creating one", e);
+    await fs.writeFile({
+      path: `${dir}${path.sep}conf.json`,
+      contents: "{}",
+    });
+  }
+} catch {
+  console.log("conf dir doesn't exist, creating one");
+  await fs.createDir(dir);
+  await fs.writeFile({
+    path: `${dir}${path.sep}conf.json`,
+    contents: "{}",
+  });
+}
+
 const adapter = new CustomAdaptor();
 // TODO: it's possible to type this.
 const db = new Low<any>(adapter);
 
 // Read data from JSON file, this will set db.data content
 await db.read();
+
+console.log("DB:", db.data);
+
+if (!db.data || Object.keys(db.data).length === 0) {
+  // conf file structure
+  db.data = {
+    json: {
+      editor: "",
+      diff: "",
+    },
+    hash: {
+      editor: "",
+    },
+  };
+  db.write();
+}
 
 export { db };
