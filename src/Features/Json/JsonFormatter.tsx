56fd82cdcc5ed1bb13df80e60d096c92dfd4a3a5
Sparkenstein <zetabytes.pp@gmail.com>
2022-02-27 20:27:21 +0530

    feat: remove monaco, use ace

--- src/Features/Json/JsonFormatter.tsx
+++ src/Features/Json/JsonFormatter.tsx
@@ -1,11 +1,7 @@
-import { Button, Checkbox, Flex, Tooltip } from "@chakra-ui/react";
-import Editor, {
-  type DiffOnMount,
-  type OnMount,
-  DiffEditor,
-} from "@monaco-editor/react";
+import { Button, Flex, Tooltip } from "@chakra-ui/react";
 import { useDebouncedCallback } from "@react-hookz/web/esm";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import AceEditor from "react-ace";
 
 import { db } from "../../utils";
 
@@ -24,8 +20,7 @@ const def = {
 };
 
 const JsonFormatter = () => {
-  const editorRef = useRef<any>(null);
-  const [diff, setDiff] = useState(false);
+  const [code, setCode] = useState(JSON.stringify(def, null, 2));
 
   const onChange = useDebouncedCallback(
     (e) => {
@@ -41,117 +36,40 @@ const JsonFormatter = () => {
     500 // maxwait ( call at least once every 500ms )
   );
 
-  const onMount: OnMount = (editor, monaco) => {
-    // console.log("Mounted", monaco);
-    // minify command
-    editor.addAction({
-      id: "minify-json",
-      label: "Minify json",
-      //FIXME: keybindings not working
-      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM],
-      // A precondition for this action.
-      precondition: undefined,
-
-      // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
-      keybindingContext: undefined,
-
-      contextMenuGroupId: "navigation",
-
-      contextMenuOrder: 1.5,
-      run: function (ed: any) {
-        ed.setValue(JSON.stringify(def));
-      },
-    });
-
-    // ref
-    editorRef.current = editor;
-  };
-
-  const diffOnMout: DiffOnMount = (editor) => {
-    editorRef.current = editor;
-  };
-
   return (
-    <Flex
-      w="full"
-      h="full"
-      gap={3}
-      alignSelf={"start"}
-      //thanks: https://stackoverflow.com/questions/54278276/improper-resizing-after-using-resize-bar
-      sx={{
-        "& div": {
-          maxWidth: "98%",
-        },
-      }}
-    >
-      {diff ? (
-        <DiffEditor
-          options={{
-            minimap: { enabled: false },
-            originalEditable: true,
-          }}
-          theme="vs-dark"
-          height={"95%"}
-          // width={diff ? "95%" : "100%"}
-          originalLanguage="json"
-          modifiedLanguage="json"
-          original={JSON.stringify(def, null, 2)}
-          modified={JSON.stringify(def, null, 2)}
-          onMount={diffOnMout}
-        />
-      ) : (
-        <Editor
-          //refer: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IEditorMinimapOptions.html
-          options={{
-            minimap: { enabled: false },
-          }}
-          defaultLanguage="json"
-          theme="vs-dark"
-          height={"95%"}
-          // width={"99%"}
-          defaultValue={JSON.stringify(def, null, 2)}
-          onMount={onMount}
-          onChange={onChange}
-        />
-      )}
-
-      <Flex gap={5} mt={10} flexDirection={"column"}>
-        <Tooltip label="calculate Diff" openDelay={500}>
-          <Checkbox
+    <Flex w="100%" h="100%" gap={3} flexDir="column">
+      <AceEditor
+        width="100%"
+        fontSize={"14px"}
+        mode="json"
+        theme="dracula"
+        onChange={onChange}
+        value={code}
+        name="editor-json"
+        editorProps={{ $blockScrolling: true }}
+      />
+      <Flex gap={5} mt={10}>
+        <Tooltip label="Alt+Shift+F" openDelay={500}>
+          <Button
             size={"sm"}
-            onChange={() => {
-              console.log("toggling");
-              setDiff(!diff);
+            onClick={() => {
+              setCode(JSON.stringify(def, null, 2));
             }}
           >
-            Diff
-          </Checkbox>
+            Format
+          </Button>
         </Tooltip>
-        {!diff && (
-          <Tooltip label="Alt+Shift+F" openDelay={500}>
-            <Button
-              size={"sm"}
-              onClick={() => {
-                editorRef.current?.setValue(JSON.stringify(def, null, 2));
-              }}
-            >
-              Format
-            </Button>
-          </Tooltip>
-        )}
 
-        {!diff && (
-          <Tooltip label="Alt+Shift+M" openDelay={500}>
-            <Button
-              size={"sm"}
-              onClick={() => {
-                editorRef.current?.setValue(JSON.stringify(def));
-              }}
-            >
-              Minify
-            </Button>
-          </Tooltip>
-        )}
+        <Tooltip label="Alt+Shift+M" openDelay={500}>
+          <Button
+            size={"sm"}
+            onClick={() => {
+              setCode(JSON.stringify(def));
+            }}
+          >
+            Minify
+          </Button>
+        </Tooltip>
       </Flex>
     </Flex>
   );
