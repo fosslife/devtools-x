d39ee0f8fda485d2c2b2bc1abb4137aa4695b9e1
Sparkenstein <zetabytes.pp@gmail.com>
2022-03-16 11:57:09 +0530

    fix(scope): update version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,15 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      #[cfg(debug_assertions)]
+      app.get_window("main").unwrap().open_devtools();
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
