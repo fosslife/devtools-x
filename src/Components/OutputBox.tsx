594acb83de58f8115b2e04ffc67b75bbf233ca19
Sparkenstein <zetabytes.pp@gmail.com>
2022-07-31 12:17:13 +0530

    refactor: reuse outputbox

---
+++ src/Components/OutputBox.tsx
@@ -0,0 +1,36 @@
+import { Button, CopyButton, Input } from "@mantine/core";
+import { clipboard } from "@tauri-apps/api";
+import { FaCopy } from "react-icons/fa";
+
+function OutputBox({ label, value }: { label: string; value: string }) {
+  return (
+    <Input.Wrapper label={label}>
+      <Input
+        size="sm"
+        rightSectionWidth={100}
+        rightSection={
+          <CopyButton value={value}>
+            {({ copied, copy }) => (
+              <Button
+                leftIcon={<FaCopy />}
+                size="xs"
+                fullWidth={true}
+                color={copied ? "teal" : "blue"}
+                onClick={() => {
+                  copy(); //  copy doesn't work but need this function for animation.
+                  clipboard.writeText(value);
+                }}
+              >
+                {copied ? "Copied" : `Copy`}
+              </Button>
+            )}
+          </CopyButton>
+        }
+        value={value}
+        readOnly
+      ></Input>
+    </Input.Wrapper>
+  );
+}
+
+export { OutputBox };
