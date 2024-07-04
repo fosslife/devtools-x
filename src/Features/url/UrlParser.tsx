91231c2d60a29097d08eeae5c55436e86da0257e
Sparkenstein <zetabytes.pp@gmail.com>
2024-07-05 01:43:46 +0530

    feat: vertical layout

--- src/Features/url/UrlParser.tsx
+++ src/Features/url/UrlParser.tsx
@@ -1,12 +1,12 @@
 import {
   Button,
-  Divider,
+  Group,
   Stack,
   Table,
   TextInput,
   Textarea,
 } from "@mantine/core";
-import { useState } from "react";
+import { FormEventHandler, useState } from "react";
 
 const sampleUrlWithQueryparams =
   "https://www.example.com?name=John&age=30&city=NewYork&country=USA&hobbies=reading&hobbies=swimming&hobbies=movies";
@@ -19,78 +19,89 @@ export default function UrlParser() {
   const [key, setKey] = useState("");
   const [value, setValue] = useState("");
 
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!key || !value) return;
+    searchParams.set(key, value);
+    setSearchParams(searchParams);
+    setUrl(
+      url.split("?")[0] +
+        "?" +
+        Array.from(searchParams)
+          .map((x) => x.join("="))
+          .join("&")
+    );
+    setKey("");
+    setValue("");
+  };
+
   return (
-    <Stack>
-      <Textarea
-        autosize
-        minRows={4}
-        placeholder={sampleUrlWithQueryparams}
-        maxRows={10}
-        value={url}
-        onChange={(e) => {
-          setUrl(e.currentTarget.value);
-          setSearchParams(
-            new URLSearchParams(e.currentTarget.value.split("?")[1])
-          );
-        }}
-      />
-      <Divider label="output" />
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Key</Table.Th>
-            <Table.Th>Value</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {Array.from(searchParams.keys()).map((key, i) => (
-            <Table.Tr key={key + i}>
-              <Table.Td>{key}</Table.Td>
-              <Table.Td>{searchParams.get(key)}</Table.Td>
-            </Table.Tr>
-          ))}
-          <Table.Tr>
-            <Table.Td>
-              <TextInput
-                value={key}
-                onChange={(e) => {
-                  let key = e.currentTarget.value;
-                  setKey(key);
-                }}
-                placeholder="Key"
-              />
-            </Table.Td>
-            <Table.Td>
-              <TextInput
-                value={value}
-                onChange={(e) => {
-                  let value = e.currentTarget.value;
-                  setValue(value);
-                }}
-                placeholder="Value"
-              />
-            </Table.Td>
-          </Table.Tr>
-        </Table.Tbody>
-      </Table>
-      <Button
-        w="fit-content"
-        onClick={() => {
-          searchParams.set(key, value);
-          setSearchParams(searchParams);
-          setUrl(
-            url.split("?")[0] +
-              "?" +
-              Array.from(searchParams)
-                .map((x) => x.join("="))
-                .join("&")
-          );
-          setKey("");
-          setValue("");
-        }}
-      >
-        Add new
-      </Button>
-    </Stack>
+    <form onSubmit={onSubmit}>
+      <Stack>
+        <Group wrap="nowrap" align="start" h="100%">
+          <Textarea
+            w={"50%"}
+            autosize
+            minRows={15}
+            placeholder={sampleUrlWithQueryparams}
+            maxRows={30}
+            value={url}
+            onChange={(e) => {
+              setUrl(e.currentTarget.value);
+              setSearchParams(
+                new URLSearchParams(e.currentTarget.value.split("?")[1])
+              );
+            }}
+          />
+          <Table w="50%" striped withColumnBorders>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Key</Table.Th>
+                <Table.Th>Value</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {Array.from(searchParams.keys()).map((key, i) => (
+                <Table.Tr key={key + i}>
+                  <Table.Td>{key}</Table.Td>
+                  <Table.Td>{searchParams.get(key)}</Table.Td>
+                </Table.Tr>
+              ))}
+              <Table.Tr>
+                <Table.Td>
+                  <TextInput
+                    value={key}
+                    onChange={(e) => {
+                      let key = e.currentTarget.value;
+                      setKey(key);
+                    }}
+                    placeholder="Key"
+                  />
+                </Table.Td>
+                <Table.Td>
+                  <TextInput
+                    value={value}
+                    onChange={(e) => {
+                      let value = e.currentTarget.value;
+                      setValue(value);
+                    }}
+                    placeholder="Value"
+                  />
+                </Table.Td>
+              </Table.Tr>
+            </Table.Tbody>
+          </Table>
+        </Group>
+        <Button
+          w="fit-content"
+          style={{
+            alignSelf: "flex-end",
+          }}
+          type="submit"
+        >
+          Add
+        </Button>
+      </Stack>
+    </form>
   );
 }
