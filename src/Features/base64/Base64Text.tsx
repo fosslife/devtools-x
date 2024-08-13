7f7f35ecfd447704ace8d77b53b39a945d9ec088
Sparkenstein <zetabytes.pp@gmail.com>
2024-08-13 09:27:30 +0530

    fix: overflow screen on multiple lines

--- src/Features/base64/Base64Text.tsx
+++ src/Features/base64/Base64Text.tsx
@@ -22,14 +22,26 @@ const Base64 = () => {
         onChange={(e) => setInput(e.target.value)}
         value={input}
         minRows={7}
+        maxRows={7}
         autosize
+        spellCheck={false}
       />
+
       <Group>
         <Button onClick={() => dataToBase64(input)}>To Base64</Button>
+
         <Button onClick={() => base64ToData(input)}>From Base64</Button>
       </Group>
 
-      <Textarea minRows={7} autosize label="Output" value={output} readOnly />
+      <Textarea
+        minRows={7}
+        maxRows={7}
+        autosize
+        label="Output"
+        value={output}
+        readOnly
+      />
+
       <Copy value={output} label="Copy" />
     </Stack>
   );
