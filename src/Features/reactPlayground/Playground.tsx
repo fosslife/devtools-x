f53f74712b2024934f66b7247755fe7243184fb9
Sparkenstein <zetabytes.pp@gmail.com>
2024-08-20 13:10:45 +0530

    feat(playground): error handling

--- src/Features/reactPlayground/Playground.tsx
+++ src/Features/reactPlayground/Playground.tsx
@@ -1,16 +1,13 @@
 import { Group, Stack } from "@mantine/core";
-import { useState } from "react";
-import { LivePreview, LiveProvider } from "react-live";
-
-import { Monaco } from "@/Components/MonacoWrapper";
+import { LivePreview, LiveProvider, LiveError, LiveEditor } from "react-live";
 
 const boilerplate = `
-// Syntax highlighting, autocompletion etc 
-// doesn't work for jsx, only TS.
-// access to entire \`React\` is auto injected
+// Syntax highlighting works. autocompletion doesn't work.
+// access to entire \`React\` is auto injected. 
 
-function App(){
-    const [count, setCount] = React.useState(0);
+function App() {
+    const [count, setCount] = React.useState<string>(0);
+
     return <div>
         <h4>Counter: {count}</h4>
         <button onClick={() => setCount(count + 1)}>add1</button>
@@ -20,67 +17,32 @@ function App(){
 `;
 
 function Playground() {
-  const [code, setCode] = useState(boilerplate);
-  // const;
   return (
     <Stack
       style={{
         height: "100%",
+        overflow: "auto",
       }}
     >
       <Group style={{ height: "100%", width: "100%" }} wrap="nowrap">
-        <LiveProvider code={code}>
-          <Monaco
-            width="50%"
-            value={code}
-            setValue={(e) => setCode(e || "")}
-            language="typescript"
-            onEditorMounted={(editor, monaco) => {
-              // extra libraries
-              monaco.languages.typescript.typescriptDefaults.addExtraLib(
-                `export declare function next() : string`,
-                "node_modules/@types/external/index.d.ts"
-              );
-
-              const model = monaco.editor.createModel(
-                code,
-                "typescript",
-                monaco.Uri.parse("file://index.jsx")
-              );
-              editor.setModel(null);
-              editor.setModel(model);
-              monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions(
-                {
-                  noSemanticValidation: true,
-                  noSyntaxValidation: true, // This line disables errors in jsx tags like <div>, etc.
-                }
-              );
-
-              monaco.languages.typescript.typescriptDefaults.setCompilerOptions(
-                {
-                  target: monaco.languages.typescript.ScriptTarget.Latest,
-                  allowNonTsExtensions: true,
-                  moduleResolution:
-                    monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-                  module: monaco.languages.typescript.ModuleKind.CommonJS,
-                  noEmit: true,
-                  typeRoots: ["node_modules/@types"],
-                  jsx: monaco.languages.typescript.JsxEmit.React,
-                  jsxFactory: "React.createElement",
-                  reactNamespace: "React",
-                  allowJs: true,
-                }
-              );
-            }}
-          />
-          <LivePreview
-            style={{
-              width: "50%",
-              color: "white",
-              padding: 15,
-              height: "100%",
-            }}
-          />
+        <LiveProvider code={boilerplate} enableTypeScript>
+          <LiveEditor style={{ width: "50%", height: "100%" }} />
+          <Stack align="start" h="100%" w="50%">
+            {" "}
+            <LiveError
+              style={{
+                color: "red",
+              }}
+            />
+            <LivePreview
+              style={{
+                width: "50%",
+                color: "white",
+                padding: 15,
+                height: "100%",
+              }}
+            />
+          </Stack>
         </LiveProvider>
       </Group>
     </Stack>
